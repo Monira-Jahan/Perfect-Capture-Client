3f68aed0e93f99e26847a87fa70acae056b147aa
Monira Jahan <monirajahan459@gmail.com>
2023-06-14 02:55:06 +0600

    Admin dashboard route verified

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -4,16 +4,16 @@ import useAxiosSecure from "./useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 
 const useAdmin=()=>{
-   const {user}=useContext(AuthContext);
-   const [axiosSecure]=useAxiosSecure();
-   const {data:isAdmin,isLoading:isAdminLoading}=useQuery({
-    queryKey:['isAdmin',user?.email],
-    queryFn:async()=>{
-        const res = await axiosSecure.get(`/users/admin/${user?.email}`);
-        console.log('is admin response',res)
-        return res.data.admin;
-    }
-   })
-   return [isAdmin,isAdminLoading]
+    const {user}=useContext(AuthContext);
+    const [axiosSecure]=useAxiosSecure();
+    const {data:isAdmin,isLoading:isAdminLoading}=useQuery({
+     queryKey:['isAdmin',user?.email],
+     queryFn:async()=>{
+         const res = await axiosSecure.get(`/users/admin/${user?.email}`);
+         console.log('is admin response',res)
+         return res.data.admin;
+     }
+    })
+    return [isAdmin,isAdminLoading]
 }
 export default useAdmin;
